12b690c4e647c1657f59e99aee6eb9eaa6c9ecf8
Tammarat <tammarat03041987@gmail.com>
2026-02-01 19:54:43 +0700

    Arrange

--- calculator3.cpp
+++ calculator3.cpp
@@ -10,6 +10,14 @@ int main() {
     printf( " ใส่ตัวเลข และ เครืองหมายคำนวน \n " ) ;
     scanf( "%s" , numstr ) ;
 
+    while (numstr[i] != '=' && numstr[i] != '\0') {
+        num = 0;
+        while (isdigit(numstr[i])) {
+            num = num * 10 + (numstr[i] - '0');
+            i++;
+        }
+    }
+
     return 0 ;
     
 }
